6bbdbc7ed5bba7fe301d14df90f55ccbdcc8a0ea
Daniil <sukhikhDaniilA@yandex.ru>
2024-06-04 13:20:24 +0300

    [MOD] Change editor update scripts

--- update.cmd
+++ update.cmd
@@ -1,13 +1,13 @@
 set gitdir=%CD%\pgit
 set path=%gitdir%\cmd;%path%
 if exist .\new_editor\ rmdir /s /Q new_editor
-if exist .\OpenPLC_Editor\ rmdir /s /Q OpenPLC_Editor
-git clone https://github.com/thiagoralves/OpenPLC_Editor
-if exist .\OpenPLC_Editor\editor\ (
-  move .\OpenPLC_Editor\editor .\new_editor
-  move .\OpenPLC_Editor\matiec\lib .\new_lib
-  copy /y .\OpenPLC_Editor\revision .\
-  rmdir /s /Q OpenPLC_Editor
+if exist .\OpenPLC-Editor-KauriIOT-Edition-\ rmdir /s /Q OpenPLC-Editor-KauriIOT-Edition-
+git clone https://github.com/Very1ucky/OpenPLC-Editor-KauriIOT-Edition-
+if exist .\OpenPLC-Editor-KauriIOT-Edition-\editor\ (
+  move .\OpenPLC-Editor-KauriIOT-Edition-\editor .\new_editor
+  move .\OpenPLC-Editor-KauriIOT-Edition-\matiec\lib .\new_lib
+  copy /y .\OpenPLC-Editor-KauriIOT-Edition-\revision .\
+  rmdir /s /Q OpenPLC-Editor-KauriIOT-Edition-
   echo "Update applied successfully"
 ) else (
   echo "Error cloning from repository!"
--- update.sh
+++ update.sh
@@ -3,17 +3,17 @@ if [ -d "./new_editor" ]
 then
     rm -Rf new_editor
 fi
-if [ -d "./OpenPLC_Editor" ]
+if [ -d "./OpenPLC-Editor-KauriIOT-Edition-" ]
 then
-    rm -Rf OpenPLC_Editor
+    rm -Rf OpenPLC-Editor-KauriIOT-Edition-
 fi
-git clone https://github.com/thiagoralves/OpenPLC_Editor
-if [ -d "./OpenPLC_Editor/editor" ]
+git clone https://github.com/thiagoralves/OpenPLC-Editor-KauriIOT-Edition-
+if [ -d "./OpenPLC-Editor-KauriIOT-Edition-/editor" ]
 then
-    mv "./OpenPLC_Editor/editor" ./new_editor
-    mv "./OpenPLC_Editor/matiec/lib" ./new_lib
-    cp ./OpenPLC_Editor/revision ./
-    rm -Rf OpenPLC_Editor
+    mv "./OpenPLC-Editor-KauriIOT-Edition-/editor" ./new_editor
+    mv "./OpenPLC-Editor-KauriIOT-Edition-/matiec/lib" ./new_lib
+    cp ./OpenPLC-Editor-KauriIOT-Edition-/revision ./
+    rm -Rf OpenPLC-Editor-KauriIOT-Edition-
     echo "Update applied successfully"
 else
     echo "Error cloning from repository!"
